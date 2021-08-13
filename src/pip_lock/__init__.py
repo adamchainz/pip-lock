d33a43155e75c96fb179e321a1a8542cd58a4096
Adam Johnson <me@adamj.eu>
2021-08-13 11:11:54 +0100

    Add type hints (#248)

--- src/pip_lock/__init__.py
+++ src/pip_lock/__init__.py
@@ -1,15 +1,16 @@
 import os
 import sys
+from typing import Dict, Iterable, List, Optional, Tuple
 
 from pip._internal.operations.freeze import freeze as pip_freeze
 
 
-def lines_from_file(filename):
+def lines_from_file(filename: str) -> List[str]:
     with open(filename) as f:
         return f.read().split("\n")
 
 
-def read_pip(filename):
+def read_pip(filename: str) -> List[str]:
     """Return lines in pip file, concatenating included requirement files."""
     lines = lines_from_file(filename)
     for line in lines:
@@ -21,7 +22,9 @@ def read_pip(filename):
     return lines
 
 
-def get_package_versions(lines, ignore_external_and_at=False):
+def get_package_versions(
+    lines: Iterable[str], ignore_external_and_at: bool = False
+) -> Dict[str, str]:
     """Return a dictionary of package versions."""
     versions = {}
     for line in lines:
@@ -46,14 +49,14 @@ def get_package_versions(lines, ignore_external_and_at=False):
     return versions
 
 
-def get_mismatches(requirements_file_path):
+def get_mismatches(requirements_file_path: str) -> Dict[str, Tuple[str, Optional[str]]]:
     """Return a dictionary of requirement mismatches."""
     pip_lines = read_pip(requirements_file_path)
 
     expected = get_package_versions(pip_lines)
     actual = get_package_versions(pip_freeze(), ignore_external_and_at=True)
 
-    mismatches = {}
+    mismatches: Dict[str, Tuple[str, Optional[str]]] = {}
     for name, version in expected.items():
         if name not in actual:
             mismatches[name] = (version, None)
@@ -65,7 +68,11 @@ def get_mismatches(requirements_file_path):
     return mismatches
 
 
-def print_errors(errors, pre_text=None, post_text=None):
+def print_errors(
+    errors: List[str],
+    pre_text: Optional[str] = None,
+    post_text: Optional[str] = None,
+) -> None:
     """Print list of errors to stderr with optional pre_text and post_text."""
     sys.stderr.write("\033[91m")  # red text
     if pre_text:
@@ -77,7 +84,7 @@ def print_errors(errors, pre_text=None, post_text=None):
     sys.stderr.write("\033[0m")
 
 
-def check_requirements(requirements_file_path, post_text=None):
+def check_requirements(requirements_file_path: str, post_text: str = "") -> None:
     """
     Print errors and exit program if there are mismatches with the requirements
     file.
